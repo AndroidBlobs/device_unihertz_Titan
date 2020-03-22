520878fbc959df436994823825a87fc34a349fd4
ShivamKumarJha <jha.shivam3@gmail.com>
2020-03-22 16:24:45 +0000

    Titan_EEA-user 9 PPR1.180610.011 root.20191205.185324 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,12 @@
+DEVICE_PATH := device/unihertz/Titan
+BOARD_VENDOR := unihertz
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2019-10-05
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
+-include vendor/unihertz/Titan/BoardConfigVendor.mk
